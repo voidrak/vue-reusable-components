016af82e2b3d9b43cea77caee6cfa73b30abf5ef
voidrak <raklovetocode@gmail.com>
2025-08-16 10:12:18 +0300

    commit

---
+++ src/Layout/MainLayout.vue
@@ -0,0 +1,30 @@
+<script setup>
+import AppSidebar from "./AppSidebar.vue";
+import AppHeader from "./AppHeader.vue";
+import { useSidebar } from "@/composables/useSidebar";
+import Backdrop from "./Backdrop.vue";
+const { isExpanded, isHovered } = useSidebar();
+</script>
+<template>
+  <div class="min-h-screen xl:flex">
+    <AppSidebar />
+    <Backdrop />
+    <div
+      class="flex-1 transition-all duration-300 ease-in-out"
+      :class="[isExpanded || isHovered ? 'lg:ml-[290px]' : 'lg:ml-[90px]']"
+    >
+      <AppHeader />
+      <div class="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import AppSidebar from "./AppSidebar.vue";
+import AppHeader from "./AppHeader.vue";
+import { useSidebar } from "@/composables/useSidebar";
+import Backdrop from "./Backdrop.vue";
+const { isExpanded, isHovered } = useSidebar();
+</script>
