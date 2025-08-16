69a82f762425f410002db9f494b49c8e146bd0ae
voidrak <raklovetocode@gmail.com>
2025-08-16 09:56:06 +0300

    feat: add HomePage component with basic structure

---
+++ src/View/HomePage.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1 class="text-red-400">Welcome to the Home Page!</h1>
+    <ul>
+      <li class="">Create New View in view folder</li>
+      <li class="">You Can Access the primary color using the below</li>
+      <li class="">primary-25: #f2f7ff;</li>
+      <li class="">primary-50: #ecf3ff;</li>
+      <li class="">primary-100: #dde9ff;</li>
+      <li class="">primary-200: #c2d6ff;</li>
+      <li class="">primary-300: #9cb9ff;</li>
+      <li class="">primary-400: #7592ff;</li>
+      <li class="">primary-500: #465fff;</li>
+      <li class="">primary-600: #3641f5;</li>
+      <li class="">primary-700: #2a31d8;</li>
+      <li class="">primary-800: #252dae;</li>
+      <li class="">primary-900: #262e89;</li>
+      <li class="">primary-950: #161950;</li>
+    </ul>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
+import HomePage from '@/View/HomePage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HomePage
+    },
+  ],
 })
 
 export default router
