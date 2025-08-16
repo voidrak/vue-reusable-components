30e23482e8ed2bdc28db1e2a28fbedd6cdb764a0
NatnaelAbebe19 <abebenatnael455@gmail.com>
2025-08-16 11:48:33 +0300

    feat: add reusable Button component and FileInput component; update routing and styles

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import HomePage from '@/View/HomePage.vue'
+import ButtonPage from '@/View/ButtonPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import FileUpload from '@/View/FileUpload.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +11,16 @@ const router = createRouter({
       name: 'Home',
       component: HomePage
     },
+    {
+      path: '/buttons',
+      name: 'Buttons',
+      component: ButtonPage
+    },
+    {
+      path: '/fileUpload',
+      name: 'FileUpload',
+      component: FileUpload
+    }
   ],
 })
 
