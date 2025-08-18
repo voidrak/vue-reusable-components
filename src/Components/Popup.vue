00f5e504afffd91b7bf24ee22ce33a02631e2705
kaleabxyz <akaleab.9363@gmail.com>
2025-08-18 09:34:52 +0300

    Component: Popup created

---
+++ src/Components/Popup.vue
@@ -0,0 +1,44 @@
+<script setup>
+    defineProps({
+        modelValue: {
+            type: Boolean, default: false,
+        },
+        title: {
+            type: String, default: "Popup Title",
+        },
+        position: {
+            type: String, default: "center",
+        }
+    })
+
+    defineEmits(["update:modelValue"])
+</script>
+
+<template>
+    <div
+        @click="$emit('update:modelValue', false)"
+        v-if="modelValue"
+        :class="[
+            `fixed inset-0 bg-black/50 flex items-center justify-center z-50 overflow-y-auto`,
+            position === 'right' ? 'items-end pr-10' : ''
+        ]">
+        <div
+            :class="[
+                'bg-white rounded-lg shadow-2xl shadow-black/50 max-w-lg w-full relative p-6',
+                position === 'right' ? 'ml-auto mr-4 self-bottom rounded-b-none' : ''
+            ]"
+            :style="position === 'right' ? { bottom: '0', right: '0' } : {}">
+            <div class="flex justify-between items-center">
+
+                <h2 class="text-xl text-gray-700 font-semibold ">{{ title }}</h2>
+                <svg 
+                @click="$emit('update:modelValue', false)"
+                class=" cursor-pointer"
+                width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <circle opacity="0.5" cx="12" cy="12" r="10" stroke="#1C274C" stroke-width="1.5"></circle> <path d="M14.5 9.50002L9.5 14.5M9.49998 9.5L14.5 14.5" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
+            </div>
+            <slot />
+        </div>
+    </div>
+</template>
+
+
--- src/View/HomePage.vue
+++ src/View/HomePage.vue
@@ -1,28 +1,95 @@
-<script setup></script>
+<script setup>
+    import Popup from '@/Components/Popup.vue'
+    import { ref } from 'vue'
+
+    const showPopupCenter = ref(false)
+    const showPopupRight = ref(false)
+
+</script>
 
 <template>
-  <div>
-    <h1 class="text-red-400 text-4xl mt-16 ml-8">Welcome to the Home Page!</h1>
-    <h1 class="text-red-400 text-2xl uppercase">
-      Do not Forget to create new branch
-    </h1>
-    <ul class="ml-48 mt-10">
-      <li class="font-bold">Create New View in view folder</li>
-      <li class="font-bold">
-        You Can Access the primary color using the below
-      </li>
-      <li class="font-semibold">primary-25: #f2f7ff;</li>
-      <li class="">primary-50: #ecf3ff;</li>
-      <li class="">primary-100: #dde9ff;</li>
-      <li class="">primary-200: #c2d6ff;</li>
-      <li class="">primary-300: #9cb9ff;</li>
-      <li class="">primary-400: #7592ff;</li>
-      <li class="">primary-500: #465fff;</li>
-      <li class="">primary-600: #3641f5;</li>
-      <li class="">primary-700: #2a31d8;</li>
-      <li class="">primary-800: #252dae;</li>
-      <li class="">primary-900: #262e89;</li>
-      <li class="">primary-950: #161950;</li>
-    </ul>
-  </div>
+    <div>
+        <div
+            class="fixed top-4 right-4 bg-blue-500 text-white px-4 py-2 rounded cursor-pointer"
+            @click="showPopupCenter = true">
+            Open Popup Center
+        </div>
+        <div
+            class="fixed top-24 right-4 bg-blue-500 text-white px-4 py-2 rounded cursor-pointer"
+            @click="showPopupRight = true">
+            Open Popup Right
+        </div>
+        <h1 class="text-red-400 text-4xl mt-16 ml-8">Welcome to the Home Page!</h1>
+        <h1 class="text-red-400 text-2xl uppercase">
+        Do not Forget to create new branch
+        </h1>
+        <ul class="ml-48 mt-10">
+        <li class="font-bold">Create New View in view folder</li>
+        <li class="font-bold">
+            You Can Access the primary color using the below
+        </li>
+        <li class="font-semibold">primary-25: #f2f7ff;</li>
+        <li class="">primary-50: #ecf3ff;</li>
+        <li class="">primary-100: #dde9ff;</li>
+        <li class="">primary-200: #c2d6ff;</li>
+        <li class="">primary-300: #9cb9ff;</li>
+        <li class="">primary-400: #7592ff;</li>
+        <li class="">primary-500: #465fff;</li>
+        <li class="">primary-600: #3641f5;</li>
+        <li class="">primary-700: #2a31d8;</li>
+        <li class="">primary-800: #252dae;</li>
+        <li class="">primary-900: #262e89;</li>
+        <li class="">primary-950: #161950;</li>
+        </ul>
+    </div>
+    <popup v-model="showPopupRight" title="Right Popup" position="right">
+        <h1 class="text-red-400 text-4xl">Welcome to the Home Page!</h1>
+        <div class="bg-white w-24 h-24">
+        Hello
+        </div>
+        <ul class="">
+        <li class="font-bold">Create New View in view folder</li>
+        <li class="font-bold">
+            You Can Access the primary color using the below
+        </li>
+        <li class="font-semibold">primary-25: #f2f7ff;</li>
+        <li class="">primary-50: #ecf3ff;</li>
+        <li class="">primary-100: #dde9ff;</li>
+        <li class="">primary-200: #c2d6ff;</li>
+        <li class="">primary-300: #9cb9ff;</li>
+        <li class="">primary-400: #7592ff;</li>
+        <li class="">primary-500: #465fff;</li>
+        <li class="">primary-600: #3641f5;</li>
+        <li class="">primary-700: #2a31d8;</li>
+        <li class="">primary-800: #252dae;</li>
+        <li class="">primary-900: #262e89;</li>
+        <li class="">primary-950: #161950;</li>
+        </ul>
+        
+    </popup>
+    <popup v-model="showPopupCenter" title="Center Popup" position="center">
+        <h1 class="text-red-400 text-4xl">Welcome to the Home Page!</h1>
+        <div class="bg-white w-24 h-24">
+        Hello
+        </div>
+        <ul class="">
+        <li class="font-bold">Create New View in view folder</li>
+        <li class="font-bold">
+            You Can Access the primary color using the below
+        </li>
+        <li class="font-semibold">primary-25: #f2f7ff;</li>
+        <li class="">primary-50: #ecf3ff;</li>
+        <li class="">primary-100: #dde9ff;</li>
+        <li class="">primary-200: #c2d6ff;</li>
+        <li class="">primary-300: #9cb9ff;</li>
+        <li class="">primary-400: #7592ff;</li>
+        <li class="">primary-500: #465fff;</li>
+        <li class="">primary-600: #3641f5;</li>
+        <li class="">primary-700: #2a31d8;</li>
+        <li class="">primary-800: #252dae;</li>
+        <li class="">primary-900: #262e89;</li>
+        <li class="">primary-950: #161950;</li>
+        </ul>
+        
+    </popup>
 </template>
